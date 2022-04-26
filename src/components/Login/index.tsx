274bb82419bd787bfd8e208ef196b3eac228abe5
jaewoong2 <jwisgenius@naver.com>
2022-04-26 21:21:28 +0900

    implement/ login

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,12 +1,49 @@
 import React from "react";
+import styled from "@emotion/styled";
+import Input from "@components/Input";
 
-type LoginProps = {} & React.DetailedHTMLProps<
+type LoginProps = {
+  userId?: string;
+  userPassword?: string;
+  onChangeUserId?: (e: React.ChangeEvent) => {};
+  onChangeUserPassword?: (e: React.ChangeEvent) => {};
+} & React.DetailedHTMLProps<
   React.FormHTMLAttributes<HTMLFormElement>,
   HTMLFormElement
 >;
 
-const Login: React.FC<LoginProps> = ({}) => {
-  return <div>index</div>;
+const Login: React.FC<LoginProps> = ({
+  userId,
+  userPassword,
+  onChangeUserId,
+  onChangeUserPassword,
+  ...props
+}) => {
+  return (
+    <Styled.Form {...props}>
+      <Input value={userId} onChange={onChangeUserId} />
+      <div className="margin--top--20" />
+      <Input value={userPassword} onChange={onChangeUserPassword} />
+    </Styled.Form>
+  );
 };
 
 export default Login;
+
+const Styled = {
+  Form: styled.form`
+    width: 480px;
+    height: 270px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    border: 1px solid #52557ec8;
+    border-radius: 7px 7px 7px 7px;
+
+    .margin--top--20 {
+      margin-top: 20px;
+    }
+  `,
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,12 @@
 import Input from "@components/Input";
+import Login from "@components/Login";
 import type { NextPage } from "next";
 import { useState } from "react";
 
 const Home: NextPage = () => {
-  const [value, setValue] = useState("");
   return (
     <div>
-      <Input
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}
-        placeholder="ID"
-        value={value}
-      />
-      <Input
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}
-        placeholder="Password"
-        value={value}
-      />
+      <Login />
     </div>
   );
 };
