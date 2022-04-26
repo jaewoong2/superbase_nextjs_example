f68c7f5097656d512762fc9974aa1402ed7d68b7
jaewoong2 <jwisgenius@naver.com>
2022-04-26 20:55:29 +0900

    change/global.css

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import styled from "@emotion/styled";
 
 type LayoutProps = {
   children: React.ReactElement | React.ReactNode | JSX.Element;
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return <div>{children}</div>;
+  return <Container>{children}</Container>;
 };
 
 export default Layout;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
